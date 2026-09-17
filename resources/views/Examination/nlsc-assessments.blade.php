e86fac4f343410789232550c5769e3fc9d11e9c5
huzaifa137 <huzaifabukenya227@gmail.com>
2026-09-17 03:38:30 +0300

    nlsc-assessment update

--- resources/views/Examination/nlsc-assessments.blade.php
+++ resources/views/Examination/nlsc-assessments.blade.php
@@ -263,8 +263,8 @@ use App\Http\Controllers\Helper;
                                         <button type="button" class="nt-action-btn btn-edit-sm edit-assessment-btn"><i class="fas fa-pen"></i> Edit</button>
                                         <button type="button" class="nt-action-btn btn-del-sm delete-assessment-btn"><i class="fas fa-trash"></i> Delete</button>
                                         @if(in_array($exam->status, ['active', 'marks_entry']))
-                                            <a href="{{ route('nlsc-assessments.marks-entry', ['examId' => $exam->id, 'classSubjectId' => $classSubject->id, 'assessmentId' => $a->id]) }}" class="btn-nt-primary" style="text-decoration:none; display:inline-block;">
-                                                <i class="fas fa-pen me-1"></i> Go to Marks Entry
+                                            <a href="{{ route('nlsc-assessments.marks-entry', ['examId' => $exam->id, 'classSubjectId' => $classSubject->id, 'assessmentId' => $a->id]) }}" class="btn btn-sm btn-primary" style="text-decoration:none; display:inline-block;">
+                                                <i class="fas fa-list-check me-1"></i> Enter Assessment Marks
                                             </a>
                                         @endif
                                     </td>
